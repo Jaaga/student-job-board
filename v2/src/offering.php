2711f6cd4b1b80c2c6833a66574593bbe07b7aeb
root <root@yatin-Compaq-Presario-CQ61-Notebook-PC.(none)>
2014-02-20 15:27:36 +0530

    added offering.php

--- v2/src/offering.php
+++ v2/src/offering.php
@@ -1,159 +1,86 @@
-<!DOCTYPE html>
+
+
+<!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+<head>
+  <meta charset="UTF-8">
+  <title>Jaaga Student Job Board</title>
+  <link rel="stylesheet" href="../css/bootstrap.css">
+  <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+  
+  <!-- Navigation Bar -->
+  <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#app-navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">in3Hrs.com</a>
+      </div> <!-- navbar-header -->
+      <div class="collapse navbar-collapse" id="app-navbar-collapse">
+        <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Find Services</a></li>
+        <li><a href="#">Start Selling</a></li>
+        <li><a href="/login.html">Signin</a></li>
+        <li>
+          <form class="navbar-form navbar-right" role="form">
+            <button type="submit" class="btn btn-success">Sign Up</button>
+          </form>
+        </li>
+      </ul>
+      </div><!-- navbar-collapse -->
+    </div><!-- container-fluid -->
+  </nav><!-- navbar -->
 
-    <title>Master page</title>
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/navbar-fixed-top.css">
-    <link rel="stylesheet" href="css/sticky-footer-navbar.css">
-     <style>
-@font-face { font-family: JuneBug; src: url('fonts/File.otf'); } 
-h7 {
-font-family: JuneBug;
-font-size: 150%
-}
-</style>
-
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-  <body>
-
-
- <!-- Fixed navbar -->
-<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index.html"><h7>in3Hrs</h7></a>
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">Notifications</a></li>
-          </ul>
-      <form class="navbar-form navbar-nav" role="search" action="searchresults.html">
-      <div class="form-group">
-        <input type="text" class="form-control" placeholder="Search">
-      </div>
-      <button type="submit" class="btn btn-sm hidden">Search</button>
-    </form>
-          <ul class="nav navbar-nav navbar-right">
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Orders<b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Current orders</a></li>
-                <li class="divider"></li>
-                <li><a href="#">History</a></li>
-                <li class="divider"></li>
-                <li><a href="#">Expenditures</a></li>
-              </ul>
-            </li>
-             <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Profile<b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Edit Profile</a></li>
-                <li class="divider"></li>
-                <li><a href="#">Logout</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
   <div class="container">
-    
-   <div class="jumbotron">
-
     <div class="row">
       
       <div class="col-md-8">
-        <h2>I will rap a 16 line verse song about anything you want for $5 <br>
-            <small>CREATED ALMOST 3 YEARS AGO</small>
-        </h2>
-        <iframe width="650" height="350" src="//www.youtube.com/embed/tpKCqp9CALQ" frameborder="0" allowfullscreen></iframe>
+       
+        <?php
+              $con = mysql_connect("localhost","root","golny23!");
+              $db = mysql_select_db("sjb");
+              $query = mysql_query('select * from offering where offer_id = 3');
+              $row = mysql_fetch_array($query);
+              echo '<h2><a href="offering.php">' . $row['title'] . '</a></h2>';
+
+        ?>
+        
+                <iframe width="650" height="350" src="//www.youtube.com/embed/tpKCqp9CALQ" frameborder="0" allowfullscreen></iframe>
         <h4>I will create your lyrics and rap you a 16 verse song for $5. It will be around 30 seconds in length. </h4>
-        <p>2 Gigs = 60 second Rap</p>
-        <p>3 Gigs = a 1 minute and 30 second Rap</p>
-        <p>5 Gigs = a Full Song, up to 2 minutes in Length</p>
-        <h3>Instructions: </h3>
-        <p>
-          Just give me a list of numbered, one sentence bullet points that you would like me to cover.
-        </p>
-        <p>
-        ($5 per [3] bullet points) <br>
-        ([3] bullet points = 30 Seconds of Rap) <br>
+          <?php
+              
+              echo '<p>' . $row['description'] . '</p>';
 
-        For Ex: $10 =  [6] bullet points (do it like this): <br>
-
-        1. This is for my website fiverr.com <br>
-        2. its a place where you do things for $5 <br>
-        3. Sign up, its easy and fun to use <br>
-        4. Become a seller and make a lil extra $$$ each mo. <br>
-        5. Like us on Facebook.com/Fiverr <br>
-        6. Follow us on twitter @Fiverr <br>
-
-        All tracks are Professionally Mixedand Mastered! <br>
-        If you provide your own lyrics, its 70 words Max <br>
-        </p>
+        ?>
+        
+        
         <button class="btn btn-lg btn-success">Give Him a Job</button>
       </div>
 
       <div class="col-md-3">
         <h1><button class="btn btn-lg btn-success">Give Him a Job</button></h1>
-        <p>
-          <img src="images/star_full.png" alt="">
-          <img src="images/star_full.png" alt="">
-          <img src="images/star_full.png" alt="">
-          <img src="images/star_full.png" alt="">
-          <img src="images/star_full.png" alt="">
-        </p>
-        <h3>91% Rating <small>4567 Votes</small></h3>
         <hr>
-        <img src="images/male_user.png" alt="" class="img-rounded">
-        Posted By <a href="#">Nathalie Portman</a>
+        <p>
+          <a href="#" class="btn btn-success btn-xs">Happy <span class="badge">56</span></a>
+          <a href="#" class="btn btn-warning btn-xs">Unhappy <span class="badge">6</span></a>
+          <a href="#" class="btn btn-danger btn-xs">Fraud <span class="badge">0</span></a>
+        </p>
+        <hr>
+        <img src="../img/yatin-avatar.jpg" alt="" class="img-circle" height="48px">
+        Posted By <a target="_blank" href="https://twitter.com/yatintaluja">Yatin Taluja</a>
       </div>
 
     </div>
+  </div>
 
-  </div>
- </div>
-  <div id="footer">
-      <p align="center">&copy; SJB </p>
-      <div align="center">
-        <ul class="footer-links">
-            <li><a href="#about">About Us</a></li>
-            <li class="mute"> . </li>
-            <li><a href="#contact">Contact Us</a></li>
-            <li class="mute"> . </li>
-            <li><a href="#">Blog</a></li>
-        </ul>
-      </div>
-  </div>
- <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+  <script src="../js/jquery.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
 </body>
 </html>
