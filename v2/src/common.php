f41e28bcaa8dc6249834f957dad6202e4f97a764
yashrajchhabra <yashrajchhabra@gmail.com>
2014-02-26 15:54:03 +0530

    removed password

--- v2/src/common.php
+++ v2/src/common.php
@@ -4,7 +4,7 @@ if($_SESSION['uniq_id']=='')
 {
 $_SESSION['uniq_id']=uniqid();
 }
-$con=mysql_connect("localhost","root","golny23!") or die(mysql_error());
+$con=mysql_connect("localhost","root","") or die(mysql_error());
 $db=mysql_select_db("sjb") or die(mysql_error());
 
 ?>
