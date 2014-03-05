222c609cf84fb1dfee5ffef5c6f6e8e533cf6bf7
yashrajchhabra <yashrajchhabra@gmail.com>
2014-03-05 16:37:23 +0530

    removed php code

--- v2/src/user-dashboard.php
+++ v2/src/user-dashboard.php
@@ -1,28 +1,4 @@
-<?php
-
-        $con1 = mysql_connect("localhost","root","") or die("Couldn't select DB");
-        $db = mysql_select_db("sjb", $con1) or die("Couldn't select database."); 
-
-        $con2 = mysql_connect("localhost","root","") or die("Couldn't select DB"); 
-        $db = mysql_select_db("sjb", $con2)  or die("Couldn't select database.");  
-
-
-
-        $con3 = mysql_connect("localhost","root","") or die("Couldn't select DB"); 
-        $db = mysql_select_db("sjb", $con3)  or die("Couldn't select database."); 
-
-        $sql = "SELECT * from user  where user_id=2 ";
-        $result = mysql_query($sql,$con1) or die(mysql_error());  
-
-
-
-       
-        
-
-
-
-        ?>
-
+<?php include('user-dash.php'); ?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -62,57 +38,54 @@
     </div><!-- container-fluid -->
   </nav><!-- navbar -->
     <?php
-        while($row1 = mysql_fetch_array($result))                           
-        {
-           
-          
-
-           
-           
-        
-?>
+        $user=getuserdata();
+    ?>
   <div class="container">
     <div class="row">
       <div class="row">
 </div>
-		<div class="col-md-6">
+    <div class="col-md-6">
             
                 
                     <a class="thumbnail pull-left" href="#">
-                        <img class="media-object" src="<?php echo $row1['picture']; ?>" height="10">
+                        <img class="media-object" src="<?php echo $user[0]; ?>" height="10">
                     </a>
+                    <? ?> 
                     <div class="media-body">
-                        <h2 class="media-heading"><?php echo  $row1['name'];  ?></h2>
-                		<p><span class="label label-info">8 Jobs</span> </p>
+                        <h2 class="media-heading"><?php echo $user[1]; ?></h2>
+                    <p><span class="label label-info">8 Jobs</span> </p>
                         <p>
                             <a href="#" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-comment"></span> Message</a>
                             <a href="#" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-heart"></span> Favorite</a>
                         </p>
                     
-                
+           
             </div>
         </div>
 
-	</div>
+  </div>
 </div>
 <div class="container">
 <h1>Offerings:</h1>
 </div>
+<?php 
+$var=getofferings();
+for($row1=0;$row1<3;$row1++)
+{ 
+  for($row=0;$row<1;$row++)
+  {
 
+  ?>
 <div class="container">
 
-	<div class="row">
-                     <?php         $query = "SELECT *  FROM offering where user_id =2 limit 4";                   
-        $sql_result=mysql_query($query,$con2) or die("Couldn't execute query 1."); 
-
-                      while($row2=mysql_fetch_array($sql_result)){ ?>
-  
-		<div class="col-md-3 col-sm-4 col-xs-6"><p><?php echo $row2['title']; ?></p><img class="img-responsive" src="<?php echo $row2['picture']; ?>" /></div>
+  <div class="row">
+                     
+    <div class="col-md-3 col-sm-4 col-xs-6"><p></p><img class="img-responsive" src="<?php echo $var[$row1][$row]; ?>" /><?php echo $var[$row1][$row+1]; ?></div>
                 
-              <?php } } ?>
+              
+<?php } }?>
 
-
-	</div>
+  </div>
    <p><a class="btn btn-primary" role="button" href="offering.php">Check Offerings</a></p>
 
 </div>
@@ -121,19 +94,13 @@
 <div class="container">
 <h1>Orders:</h1>
   
-                     <?php $query1 = "SELECT *  FROM orders where user_id =2 limit 4";                   
-        $query_result=mysql_query($query1,$con3) or die("Couldn't execute query 1."); 
-?>
-
 
     <div class="row">
-                           <?php while($row3=mysql_fetch_array($query_result)){ ?>
+                          
 
-    	<div class="col-md-3 col-sm-4 col-xs-6"><p><?php echo $row3['details'];
-                                                        echo "-";
-                                                       echo $row3['status'];?></p>
+      <div class="col-md-3 col-sm-4 col-xs-6"><p></p>
       </div>
-              <?php } ?>
+             
 
  </div>
   <p><a class="btn btn-primary" role="button" href="orders.php">Check Orders</a></p>
