f920b7e1c79453613e33804dd382daf43238a93c
yashrajchhabra <yashrajchhabra@gmail.com>
2014-03-06 16:37:18 +0530

    session added

--- v2/src/user-dash.php
+++ v2/src/user-dash.php
@@ -1,9 +1,14 @@
 <?php
+        include('common.php');
+
+    //getting value from session
+
 	 function getuserdata()
-        {	
+        {                $userid = $_SESSION['userid'];
+
         $con1 = mysql_connect("localhost","root","") or die("Couldn't select DB");
         $db = mysql_select_db("sjb", $con1) or die("Couldn't select database."); 
-        $sql = "SELECT * from user  where user_id=2 ";
+        $sql = "SELECT * from user  where user_id=$userid ";
         $result = mysql_query($sql,$con1) or die(mysql_error());  
 
         	while($row1 = mysql_fetch_array($result))                           
@@ -13,11 +18,12 @@
     	}
 
     	function getofferings()
-    	{
+    	{                $userid = $_SESSION['userid'];
+
     		$con2 = mysql_connect("localhost","root","") or die("Couldn't select DB"); 
         	$db = mysql_select_db("sjb", $con2)  or die("Couldn't select database.");  
 
-    		$query = "SELECT *  FROM offering where user_id =2 ";                   
+    		$query = "SELECT *  FROM offering where user_id =$userid ";                   
             $sql_result=mysql_query($query,$con2) or die("Couldn't execute query 1."); 
             $row2=mysql_fetch_array($sql_result);
                 $col=0;
@@ -32,11 +38,12 @@
             return $data;
     	}
     	function getorders()
-    	{
+    	{                $userid = $_SESSION['userid'];
+
             $con3 = mysql_connect("localhost","root","") or die("Couldn't select DB"); 
             $db = mysql_select_db("sjb", $con3)  or die("Couldn't select database.");  
 
-            $query2 = "SELECT *  FROM orders where user_id =2 ";                   
+            $query2 = "SELECT *  FROM orders where user_id =$userid ";                   
             $result1=mysql_query($query2,$con3) or die("Couldn't execute query 1.");
             $row3=mysql_fetch_array($result1);
                 $col=0;
