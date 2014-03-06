39cf84f5aa313a9d68332451e901a537ce5b8ec9
yashrajchhabra <yashrajchhabra@gmail.com>
2014-03-06 16:01:37 +0530

    functionality completed

--- v2/src/user-dash.php
+++ v2/src/user-dash.php
@@ -17,28 +17,41 @@
     		$con2 = mysql_connect("localhost","root","") or die("Couldn't select DB"); 
         	$db = mysql_select_db("sjb", $con2)  or die("Couldn't select database.");  
 
-    		       $query = "SELECT *  FROM offering where user_id =2 limit 4";                   
-        $sql_result=mysql_query($query,$con2) or die("Couldn't execute query 1."); 
-
-                      while($row2=mysql_fetch_array($sql_result))
-                      {
-
-                        $arr= array(array($row2['picture'],$row2['title']));
-                    }
-                    $arr2=implode($arr);
-                    echo $arr2;
-                    return $arr;
-   
+    		$query = "SELECT *  FROM offering where user_id =2 ";                   
+            $sql_result=mysql_query($query,$con2) or die("Couldn't execute query 1."); 
+            $row2=mysql_fetch_array($sql_result);
+                $col=0;
+                for($row=0;$row<4;$row++)
+                { 
+                            
+                    $data[$row][$col] = $row2['picture'];
+                    $data[$row][$col+1]=$row2['title'];
+                    $row2=mysql_fetch_array($sql_result);
+                            
+                }
+            return $data;
     	}
-
     	function getorders()
     	{
+            $con3 = mysql_connect("localhost","root","") or die("Couldn't select DB"); 
+            $db = mysql_select_db("sjb", $con3)  or die("Couldn't select database.");  
+
+            $query2 = "SELECT *  FROM orders where user_id =2 ";                   
+            $result1=mysql_query($query2,$con3) or die("Couldn't execute query 1.");
+            $row3=mysql_fetch_array($result1);
+                $col=0;
+                for($row=0;$row<4;$row++)
+                { 
+                            
+                    $data[$row][$col] = $row3['details'];
+                    $data[$row][$col+1]=$row3['status'];
+                    $row3=mysql_fetch_array($result1);
+                            
+                }
+            return $data;
 
     	}
-    
-
-
-    	?>
+?>
           
        
         
