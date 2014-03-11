4077b7e8d36810771c9ef517cbbcda91e99bcc3a
root <yatintaluja@gmail.com>
2014-03-11 15:55:43 +0530

    create offering function created

--- public/src/offerings.php
+++ public/src/offerings.php
@@ -6,18 +6,18 @@
 
 	public function get_offering()
 	{
-			//fetching user_id from session
-			$userid = $_SESSION['user_id'];
-			//selecting offering table for particular user
-			$sql_query = "SELECT * FROM offering where user_id = userid";
-			//storing query value in result
-			$sql_result = mysql_query($sql_query,$con) or die("Couldn't Execute Query")
-			//fetching data as an array from database
-			$row2 = mysql_fetch_array($sql_result);
-			//setting column
-			$col = 0;
-			//getting offering database
-			for($row=0;$row<30;$row++)
+		//fetching user_id from session
+		$userid = $_SESSION['user_id'];
+		//selecting offering table for particular user
+		$sql_query = "SELECT * FROM offering where user_id = userid";
+		//storing query value in result
+		$sql_result = mysql_query($sql_query,$con) or die("Couldn't Execute Query")
+		//fetching data as an array from database
+		$row2 = mysql_fetch_array($sql_result);
+		//setting column
+		$col = 0;
+		//getting offering database
+		for($row=0;$row<30;$row++)
 			{
 				$data[$row][$col] = $row2['offer_id'];
 				$data[$row][$col+1] = $row2['picture'];
@@ -26,8 +26,20 @@
 				$data[$row][$col+4] = $row2['date'];
 				$row2 = mysql_fetch_array($sql_result);
 			}
-			//returning the array
-			return ($data);
+		//returning the array
+		return ($data);
+	}
+
+	public function create_offering()
+	{
+		//fetching user_id from session
+		$userid = $_SESSION['user_id'];
+		//getting values from form
+		$picture = $_POST['picture'];
+		$title = $_POST['title'];
+		$description = $_POST['description'];
+		//storing query value in result
+		$sql_query = "INSERT INTO offering(user_id, title, description, picture, date) values ('$userid', '$title', '$description', '$picture', Now())";
 	}
 }
 
