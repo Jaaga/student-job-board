128a9946d01a42809461c08e4fabad028cfa56ba
root <yatintaluja@gmail.com>
2014-03-13 16:50:12 +0530

    update,delete and sort by added

--- public/src/categoryfilter.php
+++ public/src/categoryfilter.php
@@ -7,12 +7,12 @@ $userid = $_SESSION['userid'];
 	function cat_filter($var){
 		if ($var==0) 
     {
-                  $sql=mysql_query("SELECT Distinct * FROM offering ")or die(mysql_error());
+                  $sql=mysql_query("SELECT DISTINCT * FROM offering ")or die(mysql_error());
 
     }
     else
     {
-                  $sql=mysql_query("SELECT distinct * FROM offering o  inner join offer_category oc on o.offer_id=oc.offer_id where oc.category_id='$var'")or die(mysql_error());
+                  $sql=mysql_query("SELECT DISTINCT * FROM offering o  inner join offer_category oc on o.offer_id=oc.offer_id where oc.category_id='$var'")or die(mysql_error());
 
     } 
 
@@ -28,6 +28,6 @@ $userid = $_SESSION['userid'];
                 }
                 return($data);
                
-           }
+           }    
 
 ?>
--- public/src/offerings.php
+++ public/src/offerings.php
@@ -1,46 +1,90 @@
 <?php 
 	
-	class offering{
-	
 	include('common.php');
-
-	public function get_offering()
-	{
-		//fetching user_id from session
-		$userid = $_SESSION['user_id'];
-		//selecting offering table for particular user
-		$sql_query = "SELECT * FROM offering where user_id = userid";
-		//storing query value in result
-		$sql_result = mysql_query($sql_query,$con) or die("Couldn't Execute Query")
-		//fetching data as an array from database
-		$row2 = mysql_fetch_array($sql_result);
-		//setting column
-		$col = 0;
-		//getting offering database
-		for($row=0;$row<30;$row++)
-			{
-				$data[$row][$col] = $row2['offer_id'];
-				$data[$row][$col+1] = $row2['picture'];
-				$data[$row][$col+2] = $row2['title'];
-				$data[$row][$col+3] = $row2['description'];
-				$data[$row][$col+4] = $row2['date'];
-				$row2 = mysql_fetch_array($sql_result);
-			}
+	
+	class offering{
+		//function to get offering by user_id
+		public function get_offering_by_user_id($user_id,$item_num)
+		{
+			//selecting offering table for particular user
+			$sql_query = "SELECT * FROM offering where user_id = $user_id";
+			//storing query value in result
+			$sql_result = mysql_query($sql_query,$con) or die("Couldn't Execute Query")
+			//fetching data as an array from database
+			$row2 = mysql_fetch_array($sql_result);
+			//setting column
+			$col = 0;
+			//for loop to store data in 2-D array
+			for($row=0;$row<item_num;$row++)
+				{
+					$data[$row][$col] = $row2['offer_id'];
+					$data[$row][$col+1] = $row2['picture'];
+					$data[$row][$col+2] = $row2['title'];
+					$data[$row][$col+3] = $row2['description'];
+					$data[$row][$col+4] = $row2['date'];
+					$row2 = mysql_fetch_array($sql_result);
+				}
 		//returning the array
 		return ($data);
 	}
+		
 
-	public function create_offering()
-	{
-		//fetching user_id from session
-		$userid = $_SESSION['user_id'];
-		//getting values from form
-		$picture = $_POST['picture'];
-		$title = $_POST['title'];
-		$description = $_POST['description'];
-		//storing query value in result
-		$sql_query = "INSERT INTO offering(user_id, title, description, picture, date) values ('$userid', '$title', '$description', '$picture', Now())";
-	}
+		//getting offerings by category
+		public function get_offering_by_category($category_id, $item_num,){
+			//if no category_id is defined
+			if ($category_id==0) 
+	    {
+	                  $sql=mysql_query("SELECT DISTINCT * FROM offering ")or die(mysql_error());
+
+	    }
+	    	//else with category_id
+	    	else
+	    {
+	                  $sql=mysql_query("SELECT DISTINCT * FROM offering o  inner join offer_category oc on o.offer_id=oc.offer_id where oc.category_id='$category_id'")or die(mysql_error());
+
+	    } 
+
+					$col=0;
+					//fetching data in array
+		            $row2=mysql_fetch_array($sql);
+		            //for loop to store data in 2-D array
+	                for($row=0;$row<$item_num;$row++)
+	                { 
+	                            
+	                    $data[$row][$col] = $row2['offer_id'];
+						$data[$row][$col+1] = $row2['picture'];
+						$data[$row][$col+2] = $row2['title'];
+						$data[$row][$col+3] = $row2['description'];
+						$data[$row][$col+4] = $row2['date'];
+	                    $row2=mysql_fetch_array($sql);
+	                            
+	                }
+	    //returning data array
+	    return($data);
+	               
+	           }
+
+        //create new offering
+		public function create_offering($user_id,$title,$description,$picture)
+		{
+			//query for storing value in table
+			$sql_query = "INSERT INTO offering(user_id, title, description, picture, date) VALUES ('$user_id', '$title', '$description', '$picture', Now())";
+		}
+
+		//update offering
+		public function update_offering($user_id,$offer_id,$title,$description,$picture)
+		{
+			//query for update values in table
+			$sql_query = "UPDATE offering SET title = $title, description = $description, picture = $picture WHERE offer_id = $offer_id AND user_id = $user_id";
+		}
+
+		//delete offering
+		public function delete_offering($user_id,$offer_id)
+		{
+			//query for delete values from table
+			$sql_query = "DELETE FROM offering WHERE user_id = $user_id AND offer_id = $user_id";
+		}
+
 }
 
 ?>
