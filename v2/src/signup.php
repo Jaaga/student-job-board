44045ea36f372b7e7ab1d79f3f528458322b10df
Ansal <ansal@bssatech.com>
2014-03-14 10:16:27 +0530

    new signup page added. the old one's layout and css was messy

--- v2/src/signup.php
+++ v2/src/signup.php
@@ -2,16 +2,85 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
-     <link href="css/student.css" rel="stylesheet">
-    <title>Student Profile</title>
+  <title>Jaaga Student Job Board</title>
+  <link rel="stylesheet" href="../css/lib/bootstrap.css">
+  <style>
+    body {
+      margin-top: 70px;
+      margin-left: 2%;
+      margin-right: 2%;
+    }
+    #wrap{
+background-image: -ms-linear-gradient(top, #FFFFFF 0%, #D3D8E8 100%);
+/* Mozilla Firefox */ 
+background-image: -moz-linear-gradient(top, #FFFFFF 0%, #D3D8E8 100%);
+/* Opera */ 
+background-image: -o-linear-gradient(top, #FFFFFF 0%, #D3D8E8 100%);
+/* Webkit (Safari/Chrome 10) */ 
+background-image: -webkit-gradient(linear, left top, left bottom, color-stop(0, #FFFFFF), color-stop(1, #D3D8E8));
+/* Webkit (Chrome 11+) */ 
+background-image: -webkit-linear-gradient(top, #FFFFFF 0%, #D3D8E8 100%);
+/* W3C Markup, IE10 Release Preview */ 
+background-image: linear-gradient(to bottom, #FFFFFF 0%, #D3D8E8 100%);
+background-repeat: no-repeat;
+background-attachment: fixed;
+}
+legend{
+  color:#141823;
+  font-size:25px;
+  font-weight:bold;
+}
+.signup-btn {
+  background: #79bc64;
+  background-image: -webkit-linear-gradient(top, #79bc64, #578843);
+  background-image: -moz-linear-gradient(top, #79bc64, #578843);
+  background-image: -ms-linear-gradient(top, #79bc64, #578843);
+  background-image: -o-linear-gradient(top, #79bc64, #578843);
+  background-image: linear-gradient(to bottom, #79bc64, #578843);
+  -webkit-border-radius: 4;
+  -moz-border-radius: 4;
+  border-radius: 4px;
+  text-shadow: 0px 1px 0px #898a88;
+  -webkit-box-shadow: 0px 0px 0px #a4e388;
+  -moz-box-shadow: 0px 0px 0px #a4e388;
+  box-shadow: 0px 0px 0px #a4e388;
+  font-family: Arial;
+  color: #ffffff;
+  font-size: 20px;
+  padding: 10px 20px 10px 20px;
+  border: solid #3b6e22  1px;
+  text-decoration: none;
+}
 
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/navbar-fixed-top.css">
-    <link rel="stylesheet" href="css/sticky-footer-navbar.css">
+.signup-btn:hover {
+  background: #79bc64;
+  background-image: -webkit-linear-gradient(top, #79bc64, #5e7056);
+  background-image: -moz-linear-gradient(top, #79bc64, #5e7056);
+  background-image: -ms-linear-gradient(top, #79bc64, #5e7056);
+  background-image: -o-linear-gradient(top, #79bc64, #5e7056);
+  background-image: linear-gradient(to bottom, #79bc64, #5e7056);
+  text-decoration: none;
+}
+.navbar-default .navbar-brand{
+    color:#fff;
+    font-size:30px;
+    font-weight:bold;
+  }
+.form .form-control { margin-bottom: 10px; }
+@media (min-width:768px) {
+  #home{
+    margin-top:50px;
+  }
+  #home .slogan{
+    color: #0e385f;
+    line-height: 29px;
+    font-weight:bold;
+  }
+}
+  </style>
 </head>
 <body>
+  
   
   <!-- Navigation Bar -->
   <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -27,12 +96,12 @@
       </div> <!-- navbar-header -->
       <div class="collapse navbar-collapse" id="app-navbar-collapse">
         <ul class="nav navbar-nav navbar-right">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="category.php">Find Services</a></li>
-        <li><a href="offering/offer.html.php">Start Selling</a></li>
-        <li><a href="login.php">Signin</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Find Services</a></li>
+        <li><a href="#">Start Selling</a></li>
+        <li><a href="/login.html">Signin</a></li>
         <li>
-          <form class="navbar-form navbar-right" role="form" action="signup.php">
+          <form class="navbar-form navbar-right" role="form">
             <button type="submit" class="btn btn-success">Sign Up</button>
           </form>
         </li>
@@ -41,52 +110,82 @@
     </div><!-- container-fluid -->
   </nav><!-- navbar -->
 
-  <div class="container-fluid">
+  <div class="container">
     <div class="row">
-      <div class="col-md-12">
-        <h2>Signup</h2>
-	<form class="form-horizontal well" role="form" method="post">
-   <div class="form-group">
-      <label for="firstname" class="col-sm-4 control-label">First Name</label>
-      <div class="col-sm-3">
-         <input type="text" class="form-control" id="firstname" 
-            placeholder="Enter First Name"name="fname">
-      </div>
-   </div>
-   <div class="form-group">
-      <label for="lastname" class="col-sm-4 control-label">Last Name</label>
-      <div class="col-sm-3">
-         <input type="text" class="form-control" id="lastname" 
-            placeholder="Enter Last Name" name="lname">
-      </div>
-   </div>
-   <div class="form-group">
-      <label for="email" class="col-sm-4 control-label">Email</label>
-      <div class="col-sm-3">
-         <input type="email" class="form-control" id="email" 
-            placeholder="Enter Email" name="email">
-      </div>
-   </div>
-    <div class="form-group">
-      <label for="password" class="col-sm-4 control-label">Password</label>
-      <div class="col-sm-3">
-         <input type="password" class="form-control" id="password" 
-            placeholder="Enter password" name="pw">
-      </div>
-   </div>
-     <div class="form-group">
-      <label for="cpassword" class="col-sm-4 control-label">Confirm Password</label>
-      <div class="col-sm-3">
-         <input type="password" class="form-control" id="cpassword" 
-            placeholder="Confirm Password" name="cpw"><br/>
-			<input type="submit" name="submit">
-      </div>
-   </div>
-   
-   </form>
+      <div class="col-md-6" style="margin-top:50px;margin-bottom: 50px;">
+        <h2>Signup now to get all the goodness!</h2>
+        <hr>
+        <form role="form" action="userDashboard.php">
+          <div class="form-group">
+            <label for="exampleInputEmail1">Your Name</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Your Name">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+          </div>
+          
+          <button type="submit" class="btn btn-success btn-lg" style="width:100%;">Create Account Now</button>
+          <hr>
+
+          <div class="form-group" style="margin-top:10px;">
+            <label for="exampleInputEmail1">Or Connect With Google or Facebook</label>
+            <br>
+            <button type="submit" class="btn btn-danger" style="width:50%;">Signup with Google</button>
+            <button type="submit" class="btn btn-primary" style="width:49%;">Signup with Facebook</button>            
+          </div>
+        </form>
       </div>
     </div>
   </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <hr>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">    
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="col-md-3">
+        <ul class="list-unstyled">
+          <li><strong>in3Hrs.com</strong><li>
+          <li><a href="#">About us</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <ul class="list-unstyled">
+          <li><strong>Help &amp; Support</strong><li>
+          <li><a href="#">in3Hrs.com Help</a></li>
+          <li><a href="#">Frequently Asked Questions</a></li>
+          <li><a href="#">User Forum</a></li>
+        </ul>
+      </div>  
+    </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="col-md-8">
+        <a href="#">Terms of Service</a>    
+        <a href="#">Privacy</a>    
+        <a href="#">Security</a>
+      </div>
+      <div class="col-md-4">
+        <p class="muted pull-right">© 2014 Jaaga Student Job Board. All rights reserved</p>
+      </div>
+    </div>
+  </div>
+</div>
 
   <script src="../js/jquery.min.js"></script>
   <script src="../js/bootstrap.min.js"></script>
