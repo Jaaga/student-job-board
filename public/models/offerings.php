dc980791728bd341d927426986b76e4ee3274cb0
root <yatintaluja@gmail.com>
2014-03-17 16:33:10 +0530

    users, offerings and orders php files updated

--- public/models/offerings.php
+++ public/models/offerings.php
@@ -1,27 +1,27 @@
 <?php 
 	
-	include('common.php');
+	include('dbconnection.php');
 	
 	class offering{
 		//function to get offering by user_id
-		public function get_offering_by_user_id($user_id,$item_num)
+		public function get_offerings_by_user_id($user_id,$item_num)
 		{
+			//creating connection
+			$con = create_connection();
 			//selecting offering table for particular user
 			$sql_query = "SELECT * FROM offering where user_id = $user_id";
 			//storing query value in result
-			$sql_result = mysql_query($sql_query,$con) or die("Couldn't Execute Query")
+			$sql_result = mysqli_query($con,$sql_query) or die("Couldn't Execute Query");
 			//fetching data as an array from database
-			$row2 = mysql_fetch_array($sql_result);
-			//setting column
-			$col = 0;
+			$row2 = mysqli_fetch_array($sql_result);
 			//for loop to store data in 2-D array
-			for($row=0;$row<item_num;$row++)
+			for($row=0;$row<$item_num;$row++)
 				{
-					$data[$row][$col] = $row2['offer_id'];
-					$data[$row][$col+1] = $row2['picture'];
-					$data[$row][$col+2] = $row2['title'];
-					$data[$row][$col+3] = $row2['description'];
-					$data[$row][$col+4] = $row2['date'];
+					$data[$row][0] = $row2['offer_id'];
+					$data[$row][1] = $row2['picture'];
+					$data[$row][2] = $row2['title'];
+					$data[$row][3] = $row2['description'];
+					$data[$row][4] = $row2['date'];
 					$row2 = mysql_fetch_array($sql_result);
 				}
 		//returning the array
@@ -30,33 +30,34 @@
 		
 
 		//getting offerings by category
-		public function get_offering_by_category($category_id, $item_num,){
+		public function get_offerings_by_category($category_id, $item_num,){
+			//creating connection
+			$con = create_connection();
 			//if no category_id is defined
 			if ($category_id==0) 
 	    {
-	                  $sql=mysql_query("SELECT DISTINCT * FROM offering ")or die(mysql_error());
+	                  $sql=mysqli_query($con,"SELECT DISTINCT * FROM offering ")or die(mysql_error());
 
 	    }
 	    	//else with category_id
 	    	else
 	    {
-	                  $sql=mysql_query("SELECT DISTINCT * FROM offering o  inner join offer_category oc on o.offer_id=oc.offer_id where oc.category_id='$category_id'")or die(mysql_error());
+	                  $sql=mysqli_query($con,"SELECT DISTINCT * FROM offering o  inner join offer_category oc on o.offer_id=oc.offer_id where oc.category_id='$category_id'")or die(mysql_error());
 
 	    } 
-
-					$col=0;
 					//fetching data in array
-		            $row2=mysql_fetch_array($sql);
+		            $row2=mysqli_fetch_array($sql);
 		            //for loop to store data in 2-D array
 	                for($row=0;$row<$item_num;$row++)
 	                { 
 	                            
-	                    $data[$row][$col] = $row2['offer_id'];
-						$data[$row][$col+1] = $row2['picture'];
-						$data[$row][$col+2] = $row2['title'];
-						$data[$row][$col+3] = $row2['description'];
-						$data[$row][$col+4] = $row2['date'];
-	                    $row2=mysql_fetch_array($sql);
+	                    $data[$row][0] = $row2['offer_id'];
+						$data[$row][1] = $row2['picture'];
+						$data[$row][2] = $row2['title'];
+						$data[$row][3] = $row2['description'];
+						$data[$row][4] = $row2['date'];
+						$data[$row][5] =$row2['user_id'];
+	                    $row2=mysqli_fetch_array($sql);
 	                            
 	                }
 	    //returning data array
@@ -67,22 +68,28 @@
         //create new offering
 		public function create_offering($user_id,$title,$description,$picture)
 		{
+			//creating connection
+			$con = create_connection();
 			//query for storing value in table
-			$sql_query = "INSERT INTO offering(user_id, title, description, picture, date) VALUES ('$user_id', '$title', '$description', '$picture', Now())";
+			$sql_query =	mysqli_query($con,"INSERT INTO offering(user_id, title, description, picture, date) VALUES ('$user_id', '$title', '$description', '$picture', Now())");
 		}
 
 		//update offering
 		public function update_offering($user_id,$offer_id,$title,$description,$picture)
 		{
+			//creating connection
+			$con = create_connection();
 			//query for update values in table
-			$sql_query = "UPDATE offering SET title = $title, description = $description, picture = $picture WHERE offer_id = $offer_id AND user_id = $user_id";
+			$sql_query = mysqli_query($con,"UPDATE offering SET title = $title, description = $description, picture = $picture WHERE offer_id = $offer_id AND user_id = $user_id");
 		}
 
 		//delete offering
 		public function delete_offering($user_id,$offer_id)
 		{
+			//creating connection
+			$con = create_connection();
 			//query for delete values from table
-			$sql_query = "DELETE FROM offering WHERE user_id = $user_id AND offer_id = $user_id";
+			$sql_query = mysqli_query($con,"DELETE FROM offering WHERE user_id = $user_id AND offer_id = $user_id");
 		}
 
 }
--- public/models/orders.php
+++ public/models/orders.php
@@ -11,13 +11,12 @@
 				$sql=mysqli_query($con,'SELECT * FROM orders INNER JOIN offering ON orders.user_id=$userid &&  orders.offer_id=offering.offer_id ')or die(mysqli_error($con));
 	            $result=mysqli_fetch_array($sql);
 
-				$col=0;
                 for($row=0;$row<$numrows&&$result;$row++)
                 { 
                             
-                    $data[$row][$col] = $result['picture'];
-                    $data[$row][$col+1]=$result['title'];
-                    $data[$row][$col+2]=$result['status'];
+                    $data[$row][0] = $result['picture'];
+                    $data[$row][1]=$result['title'];
+                    $data[$row][2]=$result['status'];
                     $result=mysqli_fetch_array($sql);
                             
                 }
@@ -29,22 +28,24 @@
 			$con=create_connection();
 			//storing query value in result
 			$sql_query = "INSERT INTO orders(user_id, offer_id, status, details) values ('$userid', '$offerid', 'pending', '$details')";
-			mysqli_query($con,$sql_query)or die(mysqli_error($con));
+			mysqli_query($con,$sq//creating connection
+			$con = create_connection();l_query)or die(mysqli_error($con));
 	}
-	public function get_orders_by_offerid($offerid)
+	public function get_orders_by//creating connection
+			$con = create_connection();_offerid($offerid)
 	{
 		$con=create_connection();
 
 				$sql=mysqli_query($con,'SELECT * FROM orders where offer_id=$offerid')or die(mysqli_error($con));
 	            $result=mysqli_fetch_array($sql);
 
-				$col=0;
+				
                 for($row=0;$row<$numrows&&$result;$row++)
                 { 
                             
-                    $data[$row][$col] = $result['picture'];
-                    $data[$row][$col+1]=$result['title'];
-                    $data[$row][$col+2]=$result['status'];
+                    $data[$row][0] = $result['picture'];
+                    $data[$row][1]=$result['title'];
+                    $data[$row][2]=$result['status'];
                     $result=mysqli_fetch_array($sql);          
                 }
                 return($data);
