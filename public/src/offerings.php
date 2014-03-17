1967d5dfd1225cecd9122bac4f522f8c9c80b47b
root <yatintaluja@gmail.com>
2014-03-17 16:06:29 +0530

    users.php and offerings.php edited

--- public/src/offerings.php
+++ public/src/offerings.php
@@ -4,18 +4,18 @@
 	
 	class offering{
 		//function to get offering by user_id
-		public function get_offering_by_user_id($user_id,$item_num)
+		public function get_offerings_by_user_id($user_id,$item_num)
 		{
 			//selecting offering table for particular user
 			$sql_query = "SELECT * FROM offering where user_id = $user_id";
 			//storing query value in result
-			$sql_result = mysql_query($sql_query,$con) or die("Couldn't Execute Query")
+			$sql_result = mysql_query($sql_query) or die("Couldn't Execute Query");
 			//fetching data as an array from database
 			$row2 = mysql_fetch_array($sql_result);
 			//setting column
 			$col = 0;
 			//for loop to store data in 2-D array
-			for($row=0;$row<item_num;$row++)
+			for($row=0;$row<$item_num;$row++)
 				{
 					$data[$row][$col] = $row2['offer_id'];
 					$data[$row][$col+1] = $row2['picture'];
@@ -30,7 +30,7 @@
 		
 
 		//getting offerings by category
-		public function get_offering_by_category($category_id, $item_num,){
+		public function get_offerings_by_category($category_id, $item_num,){
 			//if no category_id is defined
 			if ($category_id==0) 
 	    {
@@ -56,6 +56,7 @@
 						$data[$row][$col+2] = $row2['title'];
 						$data[$row][$col+3] = $row2['description'];
 						$data[$row][$col+4] = $row2['date'];
+						$data[$row][$col+5] =$row2['user_id'];
 	                    $row2=mysql_fetch_array($sql);
 	                            
 	                }
@@ -68,21 +69,21 @@
 		public function create_offering($user_id,$title,$description,$picture)
 		{
 			//query for storing value in table
-			$sql_query = "INSERT INTO offering(user_id, title, description, picture, date) VALUES ('$user_id', '$title', '$description', '$picture', Now())";
+			$sql_query =	mysql_query("INSERT INTO offering(user_id, title, description, picture, date) VALUES ('$user_id', '$title', '$description', '$picture', Now())");
 		}
 
 		//update offering
 		public function update_offering($user_id,$offer_id,$title,$description,$picture)
 		{
 			//query for update values in table
-			$sql_query = "UPDATE offering SET title = $title, description = $description, picture = $picture WHERE offer_id = $offer_id AND user_id = $user_id";
+			$sql_query = mysql_query("UPDATE offering SET title = $title, description = $description, picture = $picture WHERE offer_id = $offer_id AND user_id = $user_id");
 		}
 
 		//delete offering
 		public function delete_offering($user_id,$offer_id)
 		{
 			//query for delete values from table
-			$sql_query = "DELETE FROM offering WHERE user_id = $user_id AND offer_id = $user_id";
+			$sql_query = mysql_query("DELETE FROM offering WHERE user_id = $user_id AND offer_id = $user_id");
 		}
 
 }
