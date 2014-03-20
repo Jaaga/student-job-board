52b6f89a9b37c458d6e88e4c3428b32819ffaf1a
prank7 <prashant.abhishek7g@gmail.com>
2014-03-20 16:22:13 +0530

    signup model

---
+++ public/models/signup.php
@@ -0,0 +1,48 @@
+<?php 
+	
+	
+	
+	class signUp
+{
+		//function to get offering by user_id
+		public function is_signedUp($email)
+		{
+			//creating connection
+			$con = create_connection();
+			//selecting offering table for particular user
+			$sql_query = "SELECT * FROM user where email ='".$email."'";
+			//storing query value in result
+			$sql_result = mysqli_query($con,$sql_query) or die("Couldn't Execute Query");
+			//fetching data as an array from database
+			$rowcount=mysqli_num_rows($sql_result);
+			echo $rowcount;
+			//for loop to store data in 2-D array
+		//returning the array
+			if($rowcount==0)
+				return FALSE;
+			else 
+				return TRUE;
+		}
+
+
+		public function new_signup($name, $email, $password)
+		{
+			//creating connection
+			$con = create_connection();
+			//selecting offering table for particular user
+			$sql_query = "INSERT INTO user (name,email,pass) VALUES ('".$name."','".$email."','".$password."')";
+			//Inserting query value in result
+			$sql_result = mysqli_query($con,$sql_query) or die("Couldn't Execute Query");
+			
+			$sql_query_for_userid="SELECT * FROM user WHERE email='".$email."'";
+
+			$sql_result_for_userid = mysqli_query($con,$sql_query_for_userid) or die("Couldn't Execute Query");
+
+			return $sql_result_for_userid;
+		
+		}
+
+		
+}
+
+?>
