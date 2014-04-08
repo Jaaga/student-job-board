1362f9a4dbdf7d8fdc7d3d3f693ea1ce0ed44d87
yashrajchhabra <yashrajchhabra@gmail.com>
2014-04-08 13:46:33 +0530

    controller and ui for orders

--- public/orders/index.php
+++ public/orders/index.php
@@ -1,6 +1,35 @@
 
 <?php
+session_start();
+include $_SERVER['DOCUMENT_ROOT'] . '/student-job-board/public/models/dbconnection.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/student-job-board/public/models/orders.php';
+$orderOffering= new orders();
 
 
+if(isset($_GET["view"]))
+{
 include 'vieworders.html.php';
+exit();
+}
+
+if(isset($_GET["order"]))
+{
+$order=$_GET["order"];
+echo $order;
+include 'order.html.php';
+exit();
+
+
+}
+if(isset($_POST['ordernow']))
+{
+	
+	$description=$_POST["description"];
+	$offerid=$_POST['offerid'];
+	$orderOffering->create_order($_SESSION['userid'],$offerid,$description);
+	header('Location: ../user/');
+	exit();
+}
+
+
 ?>
---
+++ public/orders/order.html.php
@@ -0,0 +1,154 @@
+<?php 
+  include '../models/offerings.php';
+
+  $getorder = new orders;
+  $offering_id=$order;
+  //$data = $getorder->create_order($offering_id,,);
+  $getoffer=new offering;
+  $offerdata= $getoffer->get_offers_by_offer_id($offering_id);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>in3hrs.com</title>
+      <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="">
+      <meta name="author" content="">
+
+      <!-- Le styles -->
+      <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'>
+      <link rel="stylesheet" href="css/font-awesome.min.css">
+      <!--[if IE 7]>
+      <link rel="stylesheet" href="css/font-awesome-ie7.min.css">
+      <![endif]-->
+      <link href="../css/bootplus.css" rel="stylesheet">
+      <link href="../css/bootplus-responsive.css" rel="stylesheet">
+      <link href="../css/custom.css" rel="stylesheet">
+      <link href="../css/home.css" rel="stylesheet">
+
+
+    
+    
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="ico/favicon.png">
+  </head>
+
+  <body>
+
+
+
+    <!-- NAVBAR
+    ================================================== -->
+    <div class="navbar-wrapper">
+      <!-- Wrap the .navbar in .container to center it within the absolutely positioned parent. -->
+      <div class="container">
+
+        <div class="navbar _navbar-inverse">
+          <div class="navbar-inner">
+            <!-- Responsive Navbar Part 1: Button for triggering responsive navbar (not covered in tutorial). Include responsive CSS to utilize. -->
+            <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="brand" href="#">in3hrs.com</a>
+            <!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
+            <div class="nav-collapse collapse">
+              <ul class="nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">Find Services </a></li>
+                <li><a href="#contact">Start Selling</a></li>
+                <!-- Read about Bootstrap dropdowns at http://twitter.github.com/bootstrap/javascript.html#dropdowns -->
+
+              </ul>
+                              <a class="btn  btn-success pull-right" href="#">Sign in</a>
+
+            </div><!--/.nav-collapse -->
+          </div><!-- /.navbar-inner -->
+        </div><!-- /.navbar -->
+
+      </div> <!-- /.container -->
+    </div><!-- /.navbar-wrapper -->
+
+    <div class="container movedown">
+      <div class="row ">
+        <div class="span12 card">
+          <div class="span6">
+            <h1>Place Order</h1>
+            <hr>
+            <h3>Order no: #478sbs9</h3>
+            <h4>Instructions for Client:-</h4>
+            <h5><?php echo $offerdata[4]; ?></h5>
+            <h4>What you need!</h4>
+            <form class="form" action="." method="POST" >
+            
+            <textarea name="description" placeholder="Write your needs here"></textarea><hr>
+            <button name="ordernow" type="submit" class="btn btn-primary ">Order now</button>
+            <input type="hidden" name="offerid" value="<?php echo $offerdata[2]; ?>">
+
+            </form>
+            </div>
+
+          <div class="span5">
+                        <h2><?php echo $offerdata[1]; ?></h2>
+
+            <img src="<?php echo $offerdata[0]; ?>">
+          </div>
+        </div>
+      </div>
+    </div>
+    
+
+    <div class="container">
+     
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2013 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap-transition.js"></script>
+    <script src="js/bootstrap-alert.js"></script>
+    <script src="js/bootstrap-modal.js"></script>
+    <script src="js/bootstrap-dropdown.js"></script>
+    <script src="js/bootstrap-scrollspy.js"></script>
+    <script src="js/bootstrap-tab.js"></script>
+    <script src="js/bootstrap-tooltip.js"></script>
+    <script src="js/bootstrap-popover.js"></script>
+    <script src="js/bootstrap-button.js"></script>
+    <script src="js/bootstrap-collapse.js"></script>
+    <script src="js/bootstrap-carousel.js"></script>
+    <script src="js/bootstrap-typeahead.js"></script>
+    <script>
+      !function ($) {
+        $(function(){
+          // carousel demo
+          $('#myCarousel').carousel()
+        })
+      }(window.jQuery)
+    </script>
+    <script src="js/holder/holder.js"></script>
+  </body>
+</html>
