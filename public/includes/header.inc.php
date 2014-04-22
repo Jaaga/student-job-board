7c8581ad5ded11dbbac712466dc4f5c6cb96e34d
prank7 <prashant.abhishek7g@gmail.com>
2014-04-22 16:04:14 +0530

    header fix

--- public/includes/header.inc.php
+++ public/includes/header.inc.php
@@ -8,25 +8,23 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="brand" href="../../public/">
+          <a class="brand" href="<?php if(isset($_SESSION['userid'])){echo "/student-job-board/public/user/";} else {echo "/student-job-board/public/";} ?>">
             in3Hrs.com
           </a>
           <div class="nav-collapse collapse">
             <ul class="nav">
-              <li class="active"><a href="<?php if(!isset($_SESSION['userid'])){echo $_SERVER['DOCUMENT_ROOT'] . '/student-job-board/';} if (isset($homepage)){echo ".";} else{echo "../user/";} ?>">Home</a></li>
-              <li><a href="<?php if (isset($categoriespage)){echo ".";} if(isset($homepage)){echo "./categories/";} else{echo "../categories/";} ?>">Find Services</a></li>
-              <?php if(!isset($homepage)){ ?>
-              <li><a href="<?php if (isset($offeringpage)){echo ".";} else{echo "../offering/";} ?>">Start Selling</a></li>
-              <?php } ?>
+              <?php if(isset ($_SESSION['userid'])){ ?><li <?php if(isset($userpage)){echo "class='active'"; }?>><a href="<?php echo "/student-job-board/public/user"; ?>">Home</a></li> <?php } ?>
+              <li <?php if(isset($categoriespage)){echo "class='active'"; }?>><a href="<?php echo "/student-job-board/public/categories/" ; ?>">Find Services</a></li>
+              <?php if(isset ($_SESSION['userid'])){ ?><li <?php if (isset($offeringpage)){echo "class='active'";} ?>><a href="<?php echo "/student-job-board/public/offering" ; ?>">Create Offering</a></li> <?php } ?>
+
               <?php 
               if(!isset($_SESSION['userid'])){
-              if(isset($homepage)){
-
+             
                ?> 
               <li><a class="btn  btn-success pull-right"  href="./login/">Signin</a></li>
               <li>
                   <a class="btn  btn-success pull-right" href="./signup" style="color:white;">Sign Up</a> 
-              </li> <?php }} if(isset($_SESSION['userid'])){ ?>
+              </li> <?php } if(isset($_SESSION['userid'])){ ?>
                 <li>
                   <a class="btn  btn-success pull-right" href="../logout" style="color:white;">Logout</a> 
               </li>
