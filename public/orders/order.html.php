5193e1ccd9a310c82956822620fcff2395949aad
prank7 <prashant.abhishek7g@gmail.com>
2014-04-23 18:11:03 +0530

    order fix

--- public/orders/order.html.php
+++ public/orders/order.html.php
@@ -70,15 +70,9 @@
             <textarea name="description" placeholder="Write your needs here"></textarea><hr>
             <button name="ordernow" type="submit" class="btn btn-primary ">Order now</button>
             <input type="hidden" name="offerid" value="<?php echo $offerdata[2]; ?>">
-
             </form>
-            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHZwYJKoZIhvcNAQcEoIIHWDCCB1QCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYAgJkkChMP/ymWtECcMgfaVS1XLMLk49ldDgPRnMSVOJczhJGoYXCmn48eIqtAlCKRYIi7w39in0Pr21hmXx7gqyeoz53VHLNffi1w6hXiAzaO8A8nTJQyKiN0elVltAxRt/Z0Kte9rafkOQibG3AE1lxREhR+sjXxoauICd+w6QzELMAkGBSsOAwIaBQAwgeQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIVU4icfbbtWaAgcDnnTGuqZw4CptipNjhV5O6vK+j03kBCSqQK7l9gRobGhCsO1yb2ksm3U+5JufYj6WEWr+OYGdFw/1XZ734GFhXFf3I60DSdvkt7yZb+EokUrWddfX32IGFMM8QVC5Gn9gP8yIGg7RPKIkpzZjG2sb4UAxJiu2goXXf91pNJJ8BKqJWumd8I0RPNlxnj6ApLcSnj//wv/ToFQj3EHZHHXf9gwZYJNxaHWJUezqrbc79Wco/9U82LgQvV3UDsbKj0OCgggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xNDA0MjMwOTU4NTRaMCMGCSqGSIb3DQEJBDEWBBSShTvEBe2iB/nHYOfYv6Jhv0rI8zANBgkqhkiG9w0BAQEFAASBgMEM4iMsr8hLN5OWQTvL72hpUytJ7bgYKqHYC6UGXORIq2Z32xJtu4Q9DVciT46tgHNzCvm3TukFz994hCYY3qp9KrFwu1gwiE+GXbkeJ54zUmozsUIb57Dh0tBCQfkxhIdXuqqgGVS+bk7As9faBXnUvuj3P17N9HEnyNVsdMNj-----END PKCS7-----
-">
-<input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
-</form>
+
+           
             </div>
 
           <div class="span5">
