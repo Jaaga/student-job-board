e6c5b9167fe3eb67fa37212ec4230dbb6c6ca26c
yashrajchhabra <yashrajchhabra@gmail.com>
2014-04-24 16:27:17 +0530

    added comments table

--- db/sjb.sql
+++ db/sjb.sql
@@ -2,8 +2,8 @@
 -- version 4.1.6
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 03, 2014 at 01:34 PM
+-- Host: localhost
+-- Generation Time: Apr 24, 2014 at 12:53 PM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.9
 
@@ -56,6 +56,47 @@ INSERT INTO `category` (`category_id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comment`
+--
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `user_id` int(11) NOT NULL,
+  `offer_id` int(11) NOT NULL,
+  `comment` text NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`user_id`, `offer_id`, `comment`, `time`) VALUES
+(1, 2, 'this is sparta', '2014-04-24 06:23:38'),
+(2, 1, 'this is sparta', '2014-04-24 06:36:04'),
+(0, 1, '', '2014-04-24 07:08:55'),
+(0, 1, '', '2014-04-24 07:11:54'),
+(15, 1, '', '2014-04-24 07:14:54'),
+(15, 1, 'this is yash', '2014-04-24 07:16:00'),
+(15, 1, 'yo', '2014-04-24 07:17:45'),
+(15, 1, 'my name is yash', '2014-04-24 07:17:54'),
+(1, 1, 'this is yatin', '2014-04-24 09:47:21'),
+(1, 34, 'hi this is yatin', '2014-04-24 09:49:24'),
+(1, 34, 'best deal ever', '2014-04-24 09:49:39'),
+(1, 2, 'hahah its working ', '2014-04-24 10:26:41'),
+(1, 2, 'hahah its working ', '2014-04-24 10:27:27'),
+(1, 2, 'hahah its working ', '2014-04-24 10:27:47'),
+(1, 2, 'hahah its working ', '2014-04-24 10:28:03'),
+(1, 2, 'hahah its working ', '2014-04-24 10:28:24'),
+(1, 2, 'haha its working', '2014-04-24 10:29:02'),
+(1, 2, 'haha its working', '2014-04-24 10:29:07'),
+(1, 2, 'haha its working', '2014-04-24 10:29:09'),
+(1, 2, 'haha its working', '2014-04-24 10:29:29'),
+(1, 2, 'haha its working', '2014-04-24 10:29:35'),
+(1, 46, 'wow i was impressed ', '2014-04-24 10:51:32');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `message`
 --
 
@@ -68,7 +109,7 @@ CREATE TABLE IF NOT EXISTS `message` (
   `read_status` varchar(10) DEFAULT NULL,
   `timestamp` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 --
 -- Dumping data for table `message`
@@ -84,7 +125,9 @@ INSERT INTO `message` (`id`, `thread`, `user_id`, `sender_id`, `msg`, `read_stat
 (8, 2, 2, 3, 'I hsrv\r\n', 'Yes', '2014-04-01'),
 (9, 1, 3, 2, 'ok,done', 'No', '2014-04-03'),
 (10, 2, 3, 2, 'Ok, done', 'No', '2014-04-03'),
-(11, 2, 3, 2, 'Ok cool', 'No', '2014-04-03');
+(11, 2, 3, 2, 'Ok cool', 'No', '2014-04-03'),
+(12, 3, 3, 1, 'yes this is an order', 'No', '2014-04-24'),
+(13, 4, 3, 1, '', 'No', '2014-04-24');
 
 -- --------------------------------------------------------
 
@@ -101,7 +144,7 @@ CREATE TABLE IF NOT EXISTS `offering` (
   `date` datetime NOT NULL,
   `instruction` text,
   PRIMARY KEY (`offer_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=40 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=47 ;
 
 --
 -- Dumping data for table `offering`
@@ -112,9 +155,9 @@ INSERT INTO `offering` (`offer_id`, `user_id`, `title`, `description`, `picture`
 (2, 1, 'I will do your PHP homework', 'Need help with your homework!! Contact me and will help you create your PHP files.', '../images/php.jpg', '2014-03-25 08:22:28', 'Need info. about what the functions are supposed to do'),
 (34, 3, 'I will make a 5 pae web app ', 'I will make a web app that is. max 5 pages. It will have basic fiunctionality. mainly for prototyping', '../images/download (1).jpg', '2014-03-28 13:01:50', 'Give me what your site wants'),
 (35, 3, 'I will fix worpdress issues', 'I will fix any [plugin or customise your installation', '../images/wp-banner.jpg', '2014-03-28 13:03:30', 'Give me site domain and control panel also tell me what theme to use'),
-(36, 2, 'I will test your website', 'I will do the testing', '../images/bug.jpg', '2014-03-28 15:16:07', 'I want site'),
-(38, 2, 'I will make a HTML template ', 'I will do it in simple color scheme', '../images/land.jpg', '2014-03-28 15:23:00', 'Need design'),
-(39, 2, 'I wil make a photoshop logo', 'I will do it in 3hrs', '../images/', '2014-03-30 14:34:17', '');
+(43, 12, 'I will make a wordpress site for free', 'I will make 5 pages for you.\r\nGive me what you need.', '../images/wp-banner.jpg', '2014-04-09 13:26:02', 'Color schemes and etc.'),
+(45, 2, 'this is yash raj', 'gdjitgji', '../images/css.jpg', '2014-04-21 15:34:03', 'IJICJFXDOJK'),
+(46, 15, 'I will debug your code', 'i will do this in 5 days', '../images/bug.gif', '2014-04-22 15:50:36', 'plz give me money');
 
 -- --------------------------------------------------------
 
@@ -142,7 +185,16 @@ INSERT INTO `offer_category` (`offer_id`, `category_id`) VALUES
 (37, 3),
 (38, 3),
 (39, 5),
-(39, 8);
+(39, 8),
+(40, 1),
+(41, 1),
+(42, 1),
+(43, 9),
+(44, 12),
+(45, 9),
+(44, 2),
+(45, 3),
+(46, 13);
 
 -- --------------------------------------------------------
 
@@ -159,7 +211,7 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `status` varchar(50) NOT NULL,
   `accept_order` int(2) NOT NULL,
   PRIMARY KEY (`order_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=29 ;
 
 --
 -- Dumping data for table `orders`
@@ -173,7 +225,25 @@ INSERT INTO `orders` (`order_id`, `offer_id`, `user_id`, `order_no`, `details`, 
 (6, 3, 1, '', 'hi i will do something for you', 'pending', 0),
 (7, 3, 1, '', '', 'pending', 0),
 (8, 3, 1, '', '', 'pending', 0),
-(9, 3, 1, '', '', 'pending', 0);
+(9, 3, 1, '', '', 'pending', 0),
+(10, 0, 2, '', '', 'pending', 0),
+(11, 0, 2, '', 'this is my first prder', 'pending', 0),
+(12, 0, 2, '', 'this is wat i need', 'pending', 0),
+(13, 0, 2, '', 'thi si ', 'pending', 0),
+(14, 0, 2, '', 'thi is ', 'pending', 0),
+(15, 0, 2, '', 'thi is ', 'pending', 0),
+(16, 0, 2, '', 'thi si yash', 'pending', 0),
+(18, 34, 2, '', 'thi is yash', 'pending', 0),
+(19, 1, 2, '', '', 'pending', 0),
+(20, 2, 2, '', 'this is what i neeed plzzz', 'pending', 0),
+(21, 2, 2, '', 'i need something ', 'pending', 0),
+(22, 2, 2, '', 'thi is yash', 'pending', 0),
+(23, 1, 2, '', 'this is my ordrr', 'pending', 0),
+(24, 43, 2, '', 'this is yash raj ', 'pending', 0),
+(25, 1, 2, '', '', 'pending', 0),
+(26, 1, 15, '', '', 'pending', 0),
+(27, 34, 1, '', 'yes this is an order', 'pending', 0),
+(28, 34, 1, '', '', 'pending', 0);
 
 -- --------------------------------------------------------
 
@@ -204,7 +274,7 @@ CREATE TABLE IF NOT EXISTS `thread` (
   `thread_id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`thread_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `thread`
@@ -212,7 +282,9 @@ CREATE TABLE IF NOT EXISTS `thread` (
 
 INSERT INTO `thread` (`thread_id`, `title`) VALUES
 (1, 'I will make a wordpress'),
-(2, 'I wil make sth cool');
+(2, 'I wil make sth cool'),
+(3, 'I will make a 5 pae web app '),
+(4, 'I will make a 5 pae web app ');
 
 -- --------------------------------------------------------
 
@@ -230,7 +302,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `picture` text NOT NULL,
   `paypal` varchar(20) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
 
 --
 -- Dumping data for table `user`
@@ -238,10 +310,22 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 INSERT INTO `user` (`user_id`, `name`, `email`, `pass`, `linkedin`, `about`, `picture`, `paypal`) VALUES
 (1, 'Yatin', 'yatin@sjb.in', 'yatin', 'yatin', 'Hi,\r\nI am great.', 'images/profile.jpg', 'anyhow'),
-(2, 'Yash Raj Chhabra', 'yashraj@sjb.in', 'yash', 'yash@linkedin.com', 'Hi,\r\nI am a great a great programmer.', 'images/user.jpg', 'yash@paypal.com'),
+(2, 'Yash Raj Chhabra', 'yashraj@sjb.in', 'yash', 'yash@linkin.com', 'this is yash raj', 'images/boy.png', 'paypal@paypal.com'),
 (3, 'Prashant ', 'prashant@sjb.in', 'prank', 'prank', 'Hi,\r\nI am great.', '', 'anyhow'),
 (4, 'Ansal', 'ansal@sjb.in', 'ansal', 'ansal', 'Hi,\r\nI am great.', '', 'hi'),
-(5, 'Freeman Murray', 'freeman@sjb.in', 'freeman', 'freeman@linkedin.com', 'Hi, I am the GURU', '', 'freeman@paypal.com');
+(5, 'Freeman Murray', 'freeman@sjb.in', 'freeman', 'freeman@linkedin.com', 'Hi, I am the GURU', '', 'freeman@paypal.com'),
+(6, 'yash', 'yashraj@sjb.com', 'yash', '', '', '', ''),
+(7, 'Yash Raj', 'yaraj@gmail.com', 'yashraj', '', '', '', ''),
+(8, 'Yash Raj', 'yashraj2@sjb.in', 'rash', '', '', '', ''),
+(9, 'yash', 'yashrajc@sjb.in', '1234', '', '', '', ''),
+(10, 'Yash Raj Chhabra', 'yash@gmail.com', 'slimshady', '', '', '', ''),
+(11, 'Yash Raj', 'yashrajchh@sjb.in', 'yash', '', '', 'images/boy.png', ''),
+(12, 'Sushil ', 'sushil@sjb.in', 'sushil', '', '', 'images/boy.png', ''),
+(13, 'Yash Raj', 'yashrajchhabra@sjb.in', 'yash', '', '', 'images/boy.png', ''),
+(14, 'Prashant', 'yaaj@sjb.in', 'jnjnk', '', '', 'images/boy.png', ''),
+(15, 'arpit', 'arpit@sjb.in', 'arpit', '', '', 'images/boy.png', ''),
+(16, '', '', '', '', '', 'images/boy.png', ''),
+(17, '', 'yashra@sjb.in', '', '', '', 'images/boy.png', '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
