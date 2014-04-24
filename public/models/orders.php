14156f55f8277d6e79bd3ab92fc2f0f710c07837
prank7 <prashant.abhishek7g@gmail.com>
2014-04-24 20:52:23 +0530

    fixed orders.php

--- public/models/orders.php
+++ public/models/orders.php
@@ -17,26 +17,25 @@
 	            
 
                 for($row=0;$row<$rowcount;$row++)
-                { 
-                            
+                {          
                     $data[$row][0]=$result['picture'];
                     $data[$row][1]=$result['title'];
                     $data[$row][2]=$result['status'];
                     $data[$row][3]=$result['offer_id'];
                     $data[$row][4]=$result['details'];
                    $result=mysqli_fetch_array($sql);
-
                                               
                 }
                 mysqli_close($con);
                 return($data);
    	}
+
    	//placing an order
 	public function create_order($userid,$offerid,$details)
 	{
 			$con=create_connection();
 			//storing query value in result
-			$sql_query = "INSERT INTO orders(user_id, offer_id, status, details) values ('$userid', '$offerid', 'pending', '$details')";
+			$sql_query = "INSERT INTO orders(user_id, offer_id, status, details,accept_order) values ('$userid', '$offerid', 'Pending', '$details',0)";
 			mysqli_query($con,$sql_query)or die(mysqli_error($con));
 	}
 
@@ -44,7 +43,7 @@
 	{
 		$con=create_connection();
 
-				$sql=mysqli_query($con,"SELECT * FROM orders where offer_id='$offerid'")or die(mysqli_error($con));
+				$sql=mysqli_query($con,"SELECT * FROM orders where offer_id='$offerid'") or die(mysqli_error($con));
 	            $result=mysqli_fetch_array($sql);
 
 				
@@ -60,6 +59,68 @@
                 }
                 return($data);
 	}
+
+//user here means the orders that were ordered to him
+
+
+
+   	public function get_orders_by_user($userid)
+	{
+				$con=create_connection();
+
+				$sql=mysqli_query($con,"SELECT * FROM orders  INNER JOIN offering ON orders.offer_id=offering.offer_id &&  offering.offer_id='$userid' ")or die(mysqli_error($con));
+	            
+	            $rowcount=mysqli_num_rows($sql);
+
+	            if($rowcount==0)
+	            	return 0;
+
+	            $result=mysqli_fetch_array($sql);
+	            
+
+                for($row=0;$row<$rowcount;$row++)
+                { 
+                            
+                    $data[$row][0]=$result['picture'];
+                    $data[$row][1]=$result['title'];
+                    $data[$row][2]=$result['status'];
+                    $data[$row][3]=$result['offer_id'];
+                    $data[$row][4]=$result['details'];
+                    $data[$row][5]=$result['order_id'];
+                   	$data[$row][6]=$result['user_id'];
+                   	$data[$row][7]=$result['accept_order'];
+                   $result=mysqli_fetch_array($sql);
+
+                                              
+                }
+                mysqli_close($con);
+                return($data);
+   	}
+
+   	public function acceptOrder($orderid)
+	{
+		$con=create_connection();
+
+				$sql=mysqli_query($con,"UPDATE orders SET accept_order=1 WHERE order_id='".$orderid."'") or die(mysqli_error($con));	
+	}
+
+	public function declineOrder($orderid)
+	{
+		$con=create_connection();
+
+				$sql=mysqli_query($con,"UPDATE orders SET accept_order=-1 WHERE order_id='".$orderid."'") or die(mysqli_error($con));	
+	}
+
+
+	public function setStatus($status,$orderid)
+	{
+		$con=create_connection();
+
+				$sql=mysqli_query($con,"UPDATE orders SET status='".$status."' WHERE order_id='".$orderid."'") or die(mysqli_error($con));	
+	}
+
+
+
 }
 ?>
 
