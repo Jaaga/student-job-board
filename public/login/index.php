cc20e99922ef3b401e2eb1c3098aced5f227deb0
prank7 <prashant.abhishek7g@gmail.com>
2014-04-25 12:36:54 +0530

    header to offering" "

--- public/login/index.php
+++ public/login/index.php
@@ -5,12 +5,19 @@ session_start();
 include $_SERVER['DOCUMENT_ROOT'] . '/student-job-board/public/models/dbconnection.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/student-job-board/public/models/login.php';
 
+if(isset($_GET['referral'])){
+	$order=$_GET['referral'];
+}
+
 
 
 if(isset($_POST['submit']))
 {
 	$email=$_POST['email'];
 	$password=$_POST['password'];
+	$ref=$_POST['ref'];
+	
+
 	$userLogIn=new logIn();
 	$logInStatus=$userLogIn->is_validUser($email,$password);
 	if($logInStatus==FALSE)
@@ -21,6 +28,12 @@ if(isset($_POST['submit']))
 	}
 	else
 	{
+		if(isset($ref)){
+
+			header ('Location: ../orders/?order='.$ref);
+			exit();
+		}
+
 		header ('Location: ../user/');
 		exit();
 	}
