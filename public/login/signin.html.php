372943031ced502ea195fed75ba55887bca29c20
prank7 <prashant.abhishek7g@gmail.com>
2014-04-25 12:37:36 +0530

    signin with proper redirect

--- public/login/signin.html.php
+++ public/login/signin.html.php
@@ -49,6 +49,7 @@
         <h2 class="form-signin-heading">Please sign in</h2>
         <input type="text" class="input-block-level" placeholder="Email address" name="email">
         <input type="password" class="input-block-level" placeholder="Password" name="password">
+        <?php if(isset($order)){ echo '<input type="hidden" name="ref" value="'.$order.'">'; }?>
         <label class="checkbox">
           <input type="checkbox" value="remember-me"> Remember me
         </label>
